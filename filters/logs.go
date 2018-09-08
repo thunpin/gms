00f9824c57bac49ade7ee021c3a3a624b889d23f
Thun Pin Chiu <thunpin@gmail.com>
2018-09-08 01:19:29 +0000

    removed the login

--- filters/logs.go
+++ filters/logs.go
@@ -1,41 +1,16 @@
 package filters
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/thunpin/gms"
 	"github.com/thunpin/gms/logs"
 )
 
-func LogAll(tag string) gms.Action {
-	return func(context *gms.Context, chain *gms.Chain) (interface{}, error) {
-		if !gin.IsDebugging() {
-			logIn(tag, context)
-		}
-		result, err := chain.Next(context)
-		logOut(tag, context, result, err)
-		return result, err
-	}
-}
-
-func LogIn(tag string) gms.Action {
-	return func(context *gms.Context, chain *gms.Chain) (interface{}, error) {
-		logIn(tag, context)
-		return chain.Next(context)
-	}
-}
-
-func LogOut(tag string) gms.Action {
+func Log(tag string) gms.Action {
 	return func(context *gms.Context, chain *gms.Chain) (interface{}, error) {
 		result, err := chain.Next(context)
 		logOut(tag, context, result, err)
 		return result, err
 	}
-}
-
-func logIn(tag string, context *gms.Context) {
-	data, _ := context.GetRawData()
-	header := logs.Header{tag, context.RequestId, context.UUID, context.Path}
-	logs.Instance().Info(logs.Info{header, string(data)})
 }
 
 func logOut(tag string, context *gms.Context, result interface{}, err error) {
